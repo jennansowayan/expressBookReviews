91e0cad0ce23564bb040bebbe96f7b20b1cb2281
jennan sowayan <jennansowayan@gmail.com>
2023-05-23 11:39:09 +0300

    fixed bugs

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,7 +42,7 @@ public_users.get('/', async function (req, res) {
 function getBookByIsbn(isbn) {
   return new Promise((resolve, reject) => {
     if(isbn){
-      var isbn = parseInt(isbn);
+      isbn = parseInt(isbn);
       var book = books[isbn];
       if(book){
         resolve(JSON.stringify(book));
@@ -75,8 +75,8 @@ public_users.get('/isbn/:isbn', async function (req, res) {
     return new Promise ((resolve, reject) => {
       let booksByAuthor = [];
       if(author){
-      var author = author.replace("_"," ");
-      var length = Object.keys(books).length
+      author = author.replace("_"," ");
+      var length = Object.keys(books).length;
       for (let i = 1; i < length; i++) {      
         if(books[i].author == author){
           booksByAuthor.push(books[i]);
@@ -97,8 +97,12 @@ public_users.get('/isbn/:isbn', async function (req, res) {
  }
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
-  const booksByAuthor = await getBooksByAuthor(req.params.author);
-  res.send(booksByAuthor);
+  try {
+    const booksByAuthor = await getBooksByAuthor(req.params.author);
+    res.send(booksByAuthor);
+  } catch (err) {
+    res.status(404).send(err)
+  }
 });
 
 function getBooksByTitle(title) {
